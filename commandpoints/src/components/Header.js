793aa45d88be980920afb30989564d5c069feb8e
Anthony Miller <ehitsamill@gmail.com>
2022-02-08 00:19:08 -0700

    added header, footer, and mainbody components and attached them to app.js

---
+++ commandpoints/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Magic Card Power Rankings</h1>
+        </div>
+    )
+}
+
+
+
+export default Header;
