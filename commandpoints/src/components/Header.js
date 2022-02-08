dcb4eb13bc583ca4f6634ed38d602a8bc0c9e723
Anthony Miller <ehitsamill@gmail.com>
2022-02-08 01:43:26 -0700

    more style added to header

--- commandpoints/src/components/Header.js
+++ commandpoints/src/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
+    box-sizing: border-box;
     display: flex;
     border-bottom: 2px solid black;
     justify-content:center;
     padding: 3%;
-
+    max-width: 100%;
 `
 
 const Title = styled.h1`
     font-size: 2rem;
+    line-height: 1.5;
+    font-weight: bold;
 `
 
 const Header = () => {
