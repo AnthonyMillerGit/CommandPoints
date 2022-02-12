6a6f77cf8ca1d8a793b4367843c42689ec8adab1
Anthony Miller <ehitsamill@gmail.com>
2022-02-12 07:51:46 -0700

    reverted back to hardcoded axios call

--- commandpoints/src/components/MainBody.js
+++ commandpoints/src/components/MainBody.js
@@ -2,36 +2,32 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 
+const initValues = {
+    cards:[],
+}
+
+
+
 const MainBody = () => {
-    const iState = {
-        cards:  [],
-        cardName:  ''
-    }
 
-
-    const [cards, setCards] = useState(iState.cards)
+    const [cards, setCards] = useState(initValues.cards)
 
 
     const handleChanges = e => {
-        setCards( {cards: e.target.value})
+        setCards(...cards, e.target.value)
     }
 
     const handleSubmit = e => {
-        e.preventDefault()
-        axios.get(`https://api.magicthegathering.io/v1/cards`)
-            .then(res => {
-                console.log(res.data.cards)
-                setCards(res.data.cards)
-            })
-            .catch(err => {
-                console.log(err)
+        e.preventDefault();
+        axios.get(`https://api.magicthegathering.io/v1/cards?name=ugin`)
+        .then(res => {
+            console.log(res.data.cards)
+            setCards(res.data.cards)
         })
-    }  
-
-
-
-
-
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     return (
         <div>
@@ -41,9 +37,22 @@ const MainBody = () => {
                     <input
                         name="cardName"
                         type="text"
-                        onChange={handleChanges}
+                        onChange={() => handleChanges}
                      />
                 </label>
+                <div>                   
+                    {cards.map(card => {
+                        if (card.hasOwnProperty('imageUrl')) {
+                        return (
+                            <div>
+                                <h3>{card.name}</h3>
+                                <img alt='' src={card.imageUrl}/>
+                            </div>)}
+                        else {
+                            return null
+                        }
+                    })}         
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
